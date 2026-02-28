4c356074638719c61ed4a1f39216a5d702432f8d
dhavaljasoliya <35931919+dhavaljasoliya@users.noreply.github.com>
2026-02-28 19:39:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def get_upcoming_ipos():
         return jsonify({"success": False, "error": str(e)}), 500
 
 def scrape_dashboard_ipos():
-    url = "https://www.chittorgarh.com/ipo/ipo_dashboard.asp"
+    url = "https://www.chittorgarh.com"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
     }
